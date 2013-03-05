a70520f5c5bf9eae8ad1d1a8c1c0a25c7ccb5ec4
elk <evlosev@ya.ru>
2013-03-05 20:28:10 +0400

    creating time format determiner

---
+++ dtDateFormat.py
@@ -0,0 +1,10 @@
+__author__ = 'e.losev'
+
+
+import datetime
+import re
+
+rfc2822_regexp = re.compile('[A-Z][a-z]+,\s+\d{2}\s+[A-Z][a-z]+\s+\d{2}:\d{2}:\d{2}')
+rfc3339_regexp = re.compile('\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}')
+rfc2822_format = "%a, %b %d %H:%M:%S"
+rfc3339_format = "%Y-%m-%d %H:%M:%S"
